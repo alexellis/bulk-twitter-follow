eb6c411bd5ddfbc8e267e1850446bdaed55694b6
Alex Ellis <alexellis2@gmail.com>
2016-08-24 17:03:41 +0100

    Update config.example.py
    @akalipetis this is for you.

--- config.example.py
+++ config.example.py
@@ -2,6 +2,6 @@ config = {
     "consumer_key": "",
     "consumer_secret": "",
     "access_token": "",
-    "access_token_secret": ""
+    "access_token_secret": "",
+    "appear_in_my_feed": "True"
 }
-
